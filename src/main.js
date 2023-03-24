4a4be6af5c941c8dad7acd2821846a522724ef15
叶细华 <yexh@pvc123.com>
2023-03-24 23:20:12 +0800

    添加背景音乐，选项，帮助，退出

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import "./assets/main.css"
 
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +13,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ArcoVue);
 app.mount('#app')
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="home">
+  <div class="home" @click="startMusic">
     <img src="../assets/bgc/home-page.jpg" alt="" usemap="#Map0" />
-    <map name="Map0">
+    <map name="Map0" v-if="!visable&&!helpAbel">
       <area
         href="javascript:void(0)"
         shape="rect"
@@ -21,43 +21,60 @@
         @click="exit"
       />
     </map>
+    <img src="../assets/bgc/help.png" alt="" v-show="helpAbel" class="helpimg" @click="helpClose">
     <MessageBox :visable="visable" @close="closeChange">
-      <div class="text">{{content}}</div>
+      <div class="text">{{ content }}</div>
     </MessageBox>
+    <audio autoplay loop ref="audioRef">
+      <source src="../assets/music/home.ogg" type="audio/ogg" />
+    </audio>
   </div>
 </template>
 
 <script setup>
 import MessageBox from "../components/MessageBox.vue";
-import { reactive, ref } from "vue";
-const visable=ref(false)
-const content=ref(null)
-const ShowOptions=()=>{
-   visable.value=true
-   content.value="暂无内容....."
+import { onMounted, reactive, ref } from "vue";
+
+const helpAbel=ref(false);
+const visable = ref(false);
+const content = ref(null);
+const audioRef = ref();
+const ShowOptions = () => {
+  visable.value = true;
+  content.value = "暂无内容.....";
+};
+const ShowHelp = () => {
+  helpAbel.value = true;
+};
+const helpClose=()=>{
+  helpAbel.value = false;
 }
-const ShowHelp=()=>{
-   visable.value=true
-   content.value="点击收集阳光 选择种植植物 保护好你的脑子！"
-}
-const exit=()=>{
-   visable.value=true
-   content.value="自己关闭网页 不用我教了吧"
-}
-const closeChange=()=>{
-   visable.value=false
+const exit = () => {
+  visable.value = true;
+  content.value = "自己关闭网页 不用我教了吧";
+};
+const closeChange = () => {
+  visable.value = false;
+};
+const startMusic=()=>{
+  audioRef.value.play()
 }
 </script>
 
 <style scoped>
-.home{
-   position: relative;
+.home {
+  position: relative;
 }
-.text{
-   color: white;
-   text-align: center;
-   font-size: 24px;
-   letter-spacing: 5px;
+.text {
+  color: white;
+  text-align: center;
+  font-size: 24px;
+  letter-spacing: 5px;
 }
-
+.helpimg{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
